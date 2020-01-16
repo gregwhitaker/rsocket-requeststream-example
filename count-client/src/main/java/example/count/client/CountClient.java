f5d0552ce30be02bc3c809e58d40e349ab791eb0
gregwhitaker <gwhitake@gmail.com>
2020-01-16 11:49:17 -0800

    Doing work

---
+++ count-client/src/main/java/example/count/client/CountClient.java
@@ -0,0 +1,38 @@
+package example.count.client;
+
+import io.netty.buffer.Unpooled;
+import io.rsocket.RSocket;
+import io.rsocket.RSocketFactory;
+import io.rsocket.transport.netty.client.TcpClientTransport;
+import io.rsocket.util.DefaultPayload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.math.BigInteger;
+import java.util.concurrent.CountDownLatch;
+
+public class CountClient {
+    private static final Logger LOG = LoggerFactory.getLogger(CountClient.class);
+
+    public static void main(String... args) throws Exception {
+        RSocket rSocket = RSocketFactory.connect()
+                .transport(TcpClientTransport.create(7000))
+                .start()
+                .block();
+
+        CountDownLatch latch = new CountDownLatch(10);
+
+        rSocket.requestStream(DefaultPayload.create(Unpooled.EMPTY_BUFFER))
+                .subscribe(payload -> {
+                    byte[] bytes = new byte[payload.data().readableBytes()];
+                    payload.data().readBytes(bytes);
+
+                    LOG.info("Received: {}", new BigInteger(bytes).intValue());
+
+                    latch.countDown();
+                });
+
+        latch.await();
+    }
+}
+
